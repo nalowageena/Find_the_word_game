2a900a8ed78157da78f62c9654ef7d492b08eff6
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-08-12 19:36:46 +0100

    add hints

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const toggle_dark = document.querySelector("#toggle-dark");
 
 const body = document.body;
-const modal = document.querySelector('.modal-content')
+
 
 
 function darkToggle() {
     toggle_dark.firstElementChild.classList.toggle("fa-sun");
     body.classList.toggle("light-mode");
-    modal.classList.toggle("light-mode")
 }
 
 toggle_dark.addEventListener("click", darkToggle);
--- game/app.js
+++ game/app.js
@@ -1,35 +1,69 @@
 import { words } from './words.js';
 
 let word = '';
-let word_space = document.querySelector('.word');
-let check = document.querySelector('.check');
-let points = document.querySelectorAll('.points');
-let stars = document.querySelectorAll('.stars');
+const word_space = document.querySelector('.word');
+const check = document.querySelector('.check');
+const points = document.querySelectorAll('.points');
+const stars = document.querySelectorAll('.stars');
 
-let lives = document.querySelectorAll('.life');
+const lives = document.querySelectorAll('.life');
 let livesArray = Array.from(lives);
 
-let modal = document.querySelector('.modal')
-let secretWord = document.querySelector('.secret-word');
+const modal = document.querySelector('.modal');
+const hintModal = document.querySelector('.hintModal')
 
-let restart = document.querySelector('.restart')
+
+const secretWord = document.querySelector('.secret-word');
+
+const restart = document.querySelector('.restart');
+
+const hint = document.querySelector('.hint');
+
+const hintStmt = document.querySelector('.hint-stmt')
 
 function findWord() {
-  // computer choses secret word
   do {
     word = words[Math.floor(Math.random() * words.length)];
   } while (word.includes('-'));
   return word;
 }
-word = findWord();
-console.log(word);
+
+const definition = document.querySelector('.definition');
+let definitions;
+
+function loadDefinition() {
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word, true);
+
+  request.onload = function () {
+    // begin accessing data here
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      definitions = data[0].meanings[0].definitions;
+      definition.textContent = definitions[getRandomInt(definitions.length)].definition;
+    } else {
+      console.log('error');
+    }
+  }
+  request.send();
+}
 
 function newWord() {
+  word = findWord();
+  console.log(word);
+  loadDefinition();
+}
+
+newWord();
+
+function changeInputStyle() {
   setTimeout(function () {
     check.innerHTML = '<i class="fa-solid fa-question"></i>';
     word_space.classList.remove('border-red', 'border-green');
     check.classList.remove('button-red', 'button-green');
     word_space.value = "";
+    word_space.disabled = false;
   }, 2000);
 }
 
@@ -43,16 +77,15 @@ function compareWord() {
       point.textContent = parseInt(point.textContent) + 10;
       noPoints = parseInt(point.textContent);
     });
-    if (noPoints % 100 === 0) {
+    if (noPoints % 50 === 0) {
       stars.forEach(function (star) {
         star.textContent = parseInt(star.textContent) + 1;
       });
     }
-  check.innerHTML = '<i class="fa-solid fa-check"></i>';
-  word_space.classList.add('border-green');
-  check.classList.add('button-green');
-    word = findWord();
-    console.log(word);
+    check.innerHTML = '<i class="fa-solid fa-check"></i>';
+    word_space.classList.add('border-green');
+    check.classList.add('button-green');
+    newWord();
   }
   else {
     check.innerHTML = '<i class="fa-solid fa-xmark"></i>';
@@ -60,18 +93,14 @@ function compareWord() {
     check.classList.add('button-red');
     manageLives();
   }
-  newWord();
+  word_space.disabled = true; 
+  changeInputStyle();
 }
 
 check.addEventListener('click', compareWord)
 
 let count = livesArray.length - 1;
 function manageLives() {
-  // if comparing the words gives false, lives decreases by 01
-  // color of live changes to grey
-  // if lives = 0, gameover()
-  console.log(count);
-
   if (count >= 0) {
     livesArray[count].classList.add("lost");
     count--;
@@ -81,9 +110,6 @@ function manageLives() {
   }
 }
 function gameOver() {
-  // game over
-  // modal open with you lost written on it and a button to restart game or go back to welcome page
-  // on click restart game, the page reloads
   secretWord.textContent = word;
   modal.classList.add('show');
 }
@@ -91,3 +117,41 @@ function gameOver() {
 restart.addEventListener('click', function () {
   location.reload();
 });
+
+hint.addEventListener('click', function () {
+  hintModal.classList.add('show');
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == hintModal) {
+    hintModal.classList.remove('show');
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+let i = getRandomInt(word.length)+1;
+let j;
+do {
+  j = getRandomInt(word.length)+1;
+} while (i == j);
+
+function hint1() {
+  let randomHint = word[i-1];
+  hintStmt.innerHTML = 'The letter ' + i + ' of the secret word is <span class="secret-word">'+ randomHint+ '</span>'; 
+}
+hint1();
+function hint2() {
+  let randomHint = word[j-1];
+  hintStmt.innerHTML = 'The letter ' + j + ' of the secret word is <span class="secret-word">'+ randomHint+ '</span>'; 
+}
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+left.addEventListener('click', hint1);
+right.addEventListener('click', hint2);
