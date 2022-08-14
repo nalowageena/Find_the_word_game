146b8073589c57dca1197014f67d9e94779303c1
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-08-14 14:34:12 +0100

    add case insensitive comparison

--- game/app.js
+++ game/app.js
@@ -73,7 +73,7 @@ var noPoints = 0;
 function compareWord() {
   // compare the word with the  secret word
   // return a boolean
-  if (word_space.value === word) {
+  if (word_space.value.toLowerCase() === word.toLowerCase()) {
     points.forEach(function (point) {
       point.textContent = parseInt(point.textContent) + 10;
       noPoints = parseInt(point.textContent);
